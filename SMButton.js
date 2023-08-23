1f3509a3e6a5f153c1a34260af14c3395bbbe72a
MuneimAsif <127970035+MuneimAsif@users.noreply.github.com>
2023-08-23 12:08:38 +0500

    Add files via upload

---
+++ SMButton.js
@@ -0,0 +1,5 @@
+// function SMButton(props){
+//   return <button onClick={props.buttonClick}>{props.val}</button>
+// }
+
+// export default SMButton;
---
+++ SMCard.js
@@ -0,0 +1,13 @@
+// function SMCard (props){
+//   return <div>
+//     <div>
+//       <p>Id : {props.studentId}</p>
+//       <p>Name : {props.studentName}</p>
+//       <p>Father Name : {props.studentFname}</p>
+//       <p>Education : {props.studentEdu}</p>
+//       <p>Is Student Approved ? {props.studentIsApproved ? 'Yes' : 'No'}</p>
+//       <p><hr /></p>
+//     </div>
+//   </div>
+// }
+//   export default SMCard;
